0e31dfe28fedab1361eb329b43eb56eb28ae3e2b
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2025-03-02 23:49:16 +0200

    add back end connection

---
+++ back-end/db.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$db   = 'product_db';
+$user = 'dzhemile';
+$pass = 'S3cur3'; 
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=3307;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
